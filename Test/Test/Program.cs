3f6b8ab5aba788beaaaefcdc2c5ec2c005980bca
Srinivas Gajavilli <srinivasjobapplicant@gmail.com>
2015-11-21 17:53:15 +0100

    This is first step

---
+++ Test/Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Who are you? ");
+            String name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            char alphabet = 'A';
+
+
+
+        }
+    }
+}
